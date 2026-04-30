907200de197630775ab0eb22c2696bdae8564b49
William Davies <dcdude120@gmail.com>
2026-04-30 20:01:44 +0200

    backend build

---
+++ tool/generate_bundles.ps1
@@ -0,0 +1,22 @@
+# Ensure mason_cli is installed via Dart
+dart pub global activate mason_cli
+
+# Runs mason bundle to generate bundles for all bricks within the top-level bricks directory.
+
+# Create Dart Frog Brick
+mason bundle -s git https://github.com/dart-frog-dev/dart_frog --git-path bricks/create_dart_frog -t dart -o packages/dart_frog_cli/lib/src/commands/create/templates
+
+# Development Dart Frog Server Brick
+mason bundle -s git https://github.com/dart-frog-dev/dart_frog --git-path bricks/dart_frog_dev_server -t dart -o packages/dart_frog_cli/lib/src/commands/dev/templates
+
+# Production Dart Frog Server Brick
+mason bundle -s git https://github.com/dart-frog-dev/dart_frog --git-path bricks/dart_frog_prod_server -t dart -o packages/dart_frog_cli/lib/src/commands/build/templates
+
+# Create Dart Frog Routes and Middlewares
+mason bundle -s git https://github.com/dart-frog-dev/dart_frog --git-path bricks/dart_frog_new -t dart -o packages/dart_frog_cli/lib/src/commands/new/templates
+
+# Format the generated bundles
+Set-Location -Path "packages/dart_frog_cli"
+dart pub get
+dart format .
+Set-Location -Path ".."
---
+++ tool/packages/dart_frog_cli/lib/src/commands/create/templates/create_dart_frog_bundle.dart
@@ -0,0 +1,101 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, implicit_dynamic_list_literal, implicit_dynamic_map_literal, inference_failure_on_collection_literal
+
+import 'package:mason/mason.dart';
+
+final createDartFrogBundle = MasonBundle.fromJson(<String, dynamic>{
+  "files": [
+    {
+      "path": ".gitignore",
+      "data":
+          "IyBTZWUgaHR0cHM6Ly93d3cuZGFydGxhbmcub3JnL2d1aWRlcy9saWJyYXJpZXMvcHJpdmF0ZS1maWxlcw0KDQojIEZpbGVzIGFuZCBkaXJlY3RvcmllcyBjcmVhdGVkIGJ5IHRoZSBPcGVyYXRpbmcgU3lzdGVtDQouRFNfU3RvcmUNCg0KIyBGaWxlcyBhbmQgZGlyZWN0b3JpZXMgY3JlYXRlZCBieSBwdWINCi5kYXJ0X3Rvb2wvDQoucGFja2FnZXMNCnB1YnNwZWMubG9jaw0KDQojIEZpbGVzIGFuZCBkaXJlY3RvcmllcyBjcmVhdGVkIGJ5IGRhcnRfZnJvZw0KYnVpbGQvDQouZGFydF9mcm9nDQoNCiMgVGVzdCByZWxhdGVkIGZpbGVzDQpjb3ZlcmFnZS8=",
+      "type": "text",
+    },
+    {
+      "path": ".vscode/extensions.json",
+      "data":
+          "ew0KCSJyZWNvbW1lbmRhdGlvbnMiOiBbIkRhcnRGcm9nLmRhcnQtZnJvZy1kZXYiXQ0KfQ==",
+      "type": "text",
+    },
+    {
+      "path": "analysis_options.yaml",
+      "data":
+          "aW5jbHVkZTogcGFja2FnZTpkYXJ0X2Zyb2dfbGludC9yZWNvbW1lbmRlZC55YW1s",
+      "type": "text",
+    },
+    {
+      "path": "pubspec.yaml",
+      "data":
+          "bmFtZToge3tuYW1lLnNuYWtlQ2FzZSgpfX0NCmRlc2NyaXB0aW9uOiBBIG5ldyBEYXJ0IEZyb2cgYXBwbGljYXRpb24NCnZlcnNpb246IDEuMC4wKzENCnB1Ymxpc2hfdG86IG5vbmUNCg0KZW52aXJvbm1lbnQ6DQogIHNkazogXjMuMTEuMA0KDQpkZXBlbmRlbmNpZXM6DQogIGRhcnRfZnJvZzogXjEuMS4wDQoNCmRldl9kZXBlbmRlbmNpZXM6DQogIGRhcnRfZnJvZ19saW50OiBeMC4xLjANCiAgbW9ja3RhaWw6IF4xLjAuMA0KICB0ZXN0OiBeMS4wLjANCg==",
+      "type": "text",
+    },
+    {
+      "path": "README.md",
+      "data":
+          "IyB7e25hbWUuc25ha2VDYXNlKCl9fQ0KDQpbIVtzdHlsZTogZGFydCBmcm9nIGxpbnRdW2RhcnRfZnJvZ19saW50X2JhZGdlXV1bZGFydF9mcm9nX2xpbnRfbGlua10NClshW0xpY2Vuc2U6IE1JVF1bbGljZW5zZV9iYWRnZV1dW2xpY2Vuc2VfbGlua10NClshW1Bvd2VyZWQgYnkgRGFydCBGcm9nXShodHRwczovL2ltZy5zaGllbGRzLmlvL2VuZHBvaW50P3VybD1odHRwczovL3Rpbnl1cmwuY29tL2RhcnRmcm9nLWJhZGdlKV0oaHR0cHM6Ly9kYXJ0LWZyb2cuZGV2KQ0KDQpBbiBleGFtcGxlIGFwcGxpY2F0aW9uIGJ1aWx0IHdpdGggZGFydF9mcm9nDQoNCltkYXJ0X2Zyb2dfbGludF9iYWRnZV06IGh0dHBzOi8vaW1nLnNoaWVsZHMuaW8vYmFkZ2Uvc3R5bGUtZGFydF9mcm9nX2xpbnQtMURGOUQyLnN2Zw0KW2RhcnRfZnJvZ19saW50X2xpbmtdOiBodHRwczovL3B1Yi5kZXYvcGFja2FnZXMvZGFydF9mcm9nX2xpbnQNCltsaWNlbnNlX2JhZGdlXTogaHR0cHM6Ly9pbWcuc2hpZWxkcy5pby9iYWRnZS9saWNlbnNlLU1JVC1ibHVlLnN2Zw0KW2xpY2Vuc2VfbGlua106IGh0dHBzOi8vb3BlbnNvdXJjZS5vcmcvbGljZW5zZXMvTUlU",
+      "type": "text",
+    },
+    {
+      "path": "routes/index.dart",
+      "data":
+          "aW1wb3J0ICdwYWNrYWdlOmRhcnRfZnJvZy9kYXJ0X2Zyb2cuZGFydCc7DQoNClJlc3BvbnNlIG9uUmVxdWVzdChSZXF1ZXN0Q29udGV4dCBjb250ZXh0KSB7DQogIHJldHVybiBSZXNwb25zZShib2R5OiAnV2VsY29tZSB0byBEYXJ0IEZyb2chJyk7DQp9DQo=",
+      "type": "text",
+    },
+    {
+      "path": "test/routes/index_test.dart",
+      "data":
+          "aW1wb3J0ICdkYXJ0OmlvJzsNCg0KaW1wb3J0ICdwYWNrYWdlOmRhcnRfZnJvZy9kYXJ0X2Zyb2cuZGFydCc7DQppbXBvcnQgJ3BhY2thZ2U6bW9ja3RhaWwvbW9ja3RhaWwuZGFydCc7DQppbXBvcnQgJ3BhY2thZ2U6dGVzdC90ZXN0LmRhcnQnOw0KDQppbXBvcnQgJy4uLy4uL3JvdXRlcy9pbmRleC5kYXJ0JyBhcyByb3V0ZTsNCg0KY2xhc3MgX01vY2tSZXF1ZXN0Q29udGV4dCBleHRlbmRzIE1vY2sgaW1wbGVtZW50cyBSZXF1ZXN0Q29udGV4dCB7fQ0KDQp2b2lkIG1haW4oKSB7DQogIGdyb3VwKCdHRVQgLycsICgpIHsNCiAgICB0ZXN0KCdyZXNwb25kcyB3aXRoIGEgMjAwIGFuZCAiV2VsY29tZSB0byBEYXJ0IEZyb2chIi4nLCAoKSB7DQogICAgICBmaW5hbCBjb250ZXh0ID0gX01vY2tSZXF1ZXN0Q29udGV4dCgpOw0KICAgICAgZmluYWwgcmVzcG9uc2UgPSByb3V0ZS5vblJlcXVlc3QoY29udGV4dCk7DQogICAgICBleHBlY3QocmVzcG9uc2Uuc3RhdHVzQ29kZSwgZXF1YWxzKEh0dHBTdGF0dXMub2spKTsNCiAgICAgIGV4cGVjdChyZXNwb25zZS5ib2R5KCksIGNvbXBsZXRpb24oZXF1YWxzKCdXZWxjb21lIHRvIERhcnQgRnJvZyEnKSkpOw0KICAgIH0pOw0KICB9KTsNCn0NCg==",
+      "type": "text",
+    },
+  ],
+  "hooks": [
+    {
+      "path": "post_gen.dart",
+      "data":
+          "aW1wb3J0ICdkYXJ0OmlvJzsNCmltcG9ydCAncGFja2FnZTptYXNvbi9tYXNvbi5kYXJ0JzsNCmltcG9ydCAncGFja2FnZTpwYXRoL3BhdGguZGFydCcgYXMgcGF0aDsNCg0KRnV0dXJlPHZvaWQ+IHJ1bihIb29rQ29udGV4dCBjb250ZXh0KSBhc3luYyB7DQogIGZpbmFsIHByb2plY3REaXJlY3RvcnkgPSBwYXRoLmNhbm9uaWNhbGl6ZSgNCiAgICBjb250ZXh0LnZhcnNbJ291dHB1dF9kaXJlY3RvcnknXSBhcyBTdHJpbmc/ID8/IERpcmVjdG9yeS5jdXJyZW50LnBhdGgsDQogICk7DQogIGZpbmFsIHByb2dyZXNzID0gY29udGV4dC5sb2dnZXIucHJvZ3Jlc3MoJ0luc3RhbGxpbmcgZGVwZW5kZW5jaWVzJyk7DQogIGF3YWl0IFByb2Nlc3MucnVuKA0KICAgICdkYXJ0JywNCiAgICBbJ3B1YicsICdnZXQnXSwNCiAgICBydW5JblNoZWxsOiB0cnVlLA0KICAgIHdvcmtpbmdEaXJlY3Rvcnk6IHByb2plY3REaXJlY3RvcnksDQogICk7DQogIHByb2dyZXNzLmNvbXBsZXRlKCk7DQoNCiAgY29udGV4dC5sb2dnZXINCiAgICAuLmluZm8oJycpDQogICAgLi5zdWNjZXNzKCdDcmVhdGVkICR7Y29udGV4dC52YXJzWyduYW1lJ119IGF0ICRwcm9qZWN0RGlyZWN0b3J5LicpDQogICAgLi5pbmZvKCcnKQ0KICAgIC4uaW5mbygnR2V0IHN0YXJ0ZWQgYnkgdHlwaW5nOicpDQogICAgLi5pbmZvKCcnKQ0KICAgIC4uaW5mbygnJHtsaWdodEN5YW4ud3JhcCgnY2QnKX0gJHByb2plY3REaXJlY3RvcnknKQ0KICAgIC4uaW5mbygnJHtsaWdodEN5YW4ud3JhcCgnZGFydF9mcm9nIGRldicpfScpOw0KfQ0K",
+      "type": "text",
+    },
+    {
+      "path": "pubspec.yaml",
+      "data":
+          "bmFtZTogY3JlYXRlX2RhcnRfZnJvZ19ob29rcw0KDQplbnZpcm9ubWVudDoNCiAgc2RrOiAiPj0zLjAuMCA8NC4wLjAiDQoNCmRlcGVuZGVuY2llczoNCiAgbWFzb246IF4wLjEuMA0KICBwYXRoOiBeMS44LjENCg0KZGV2X2RlcGVuZGVuY2llczoNCiAgbW9ja3RhaWw6IF4xLjAuMA0KICB0ZXN0OiBeMS4yNS4wDQo=",
+      "type": "text",
+    },
+    {
+      "path": "test/post_gen_test.dart",
+      "data":
+          "aW1wb3J0ICdkYXJ0OmlvJzsNCg0KaW1wb3J0ICdwYWNrYWdlOm1hc29uL21hc29uLmRhcnQnOw0KaW1wb3J0ICdwYWNrYWdlOm1vY2t0YWlsL21vY2t0YWlsLmRhcnQnOw0KaW1wb3J0ICdwYWNrYWdlOnRlc3QvdGVzdC5kYXJ0JzsNCg0KaW1wb3J0ICcuLi9wb3N0X2dlbi5kYXJ0JzsNCg0KY2xhc3MgX0Zha2VIb29rQ29udGV4dCBleHRlbmRzIEZha2UgaW1wbGVtZW50cyBIb29rQ29udGV4dCB7DQogIF9GYWtlSG9va0NvbnRleHQoe0xvZ2dlcj8gbG9nZ2VyfSkgOiBfbG9nZ2VyID0gbG9nZ2VyID8/IF9Nb2NrTG9nZ2VyKCk7DQoNCiAgZmluYWwgTG9nZ2VyIF9sb2dnZXI7DQoNCiAgdmFyIF92YXJzID0gPFN0cmluZywgZHluYW1pYz57fTsNCg0KICBAb3ZlcnJpZGUNCiAgTWFwPFN0cmluZywgZHluYW1pYz4gZ2V0IHZhcnMgPT4gX3ZhcnM7DQoNCiAgQG92ZXJyaWRlDQogIHNldCB2YXJzKE1hcDxTdHJpbmcsIGR5bmFtaWM+IHZhbHVlKSA9PiBfdmFycyA9IHZhbHVlOw0KDQogIEBvdmVycmlkZQ0KICBMb2dnZXIgZ2V0IGxvZ2dlciA9PiBfbG9nZ2VyOw0KfQ0KDQpjbGFzcyBfTW9ja0xvZ2dlciBleHRlbmRzIE1vY2sgaW1wbGVtZW50cyBMb2dnZXIge30NCg0KY2xhc3MgX01vY2tQcm9ncmVzcyBleHRlbmRzIE1vY2sgaW1wbGVtZW50cyBQcm9ncmVzcyB7fQ0KDQp2b2lkIG1haW4oKSB7DQogIGdyb3VwKCdwb3N0R2VuJywgKCkgew0KICAgIGxhdGUgSG9va0NvbnRleHQgY29udGV4dDsNCiAgICBsYXRlIExvZ2dlciBsb2dnZXI7DQoNCiAgICBzZXRVcCgoKSB7DQogICAgICBsb2dnZXIgPSBfTW9ja0xvZ2dlcigpOw0KICAgICAgY29udGV4dCA9IF9GYWtlSG9va0NvbnRleHQobG9nZ2VyOiBsb2dnZXIpOw0KDQogICAgICB3aGVuKCgpID0+IGxvZ2dlci5wcm9ncmVzcyhhbnkoKSkpLnRoZW5SZXR1cm4oX01vY2tQcm9ncmVzcygpKTsNCiAgICB9KTsNCg0KICAgIHRlc3QoJ3J1biBjb21wbGV0ZXMgd2hlbiBwdWJzcGVjLnlhbWwgZXhpc3RzJywgKCkgYXN5bmMgew0KICAgICAgY29uc3QgbmFtZSA9ICdleGFtcGxlJzsNCiAgICAgIGZpbmFsIHByb2plY3REaXJlY3RvcnkgPSBEaXJlY3RvcnkuY3VycmVudC5wYXRoOw0KICAgICAgY29udGV4dC52YXJzID0geyduYW1lJzogbmFtZX07DQogICAgICBhd2FpdCBleHBlY3RMYXRlcihydW4oY29udGV4dCksIGNvbXBsZXRlcyk7DQogICAgICB2ZXJpZnlJbk9yZGVyKFsNCiAgICAgICAgKCkgPT4gbG9nZ2VyLmluZm8oJycpLA0KICAgICAgICAoKSA9PiBsb2dnZXINCiAgICAgICAgICAgIC5zdWNjZXNzKCdDcmVhdGVkICR7Y29udGV4dC52YXJzWyduYW1lJ119IGF0ICRwcm9qZWN0RGlyZWN0b3J5LicpLA0KICAgICAgICAoKSA9PiBsb2dnZXIuaW5mbygnJyksDQogICAgICAgICgpID0+IGxvZ2dlci5pbmZvKCdHZXQgc3RhcnRlZCBieSB0eXBpbmc6JyksDQogICAgICAgICgpID0+IGxvZ2dlci5pbmZvKCcnKSwNCiAgICAgICAgKCkgPT4gbG9nZ2VyLmluZm8oJyR7bGlnaHRDeWFuLndyYXAoJ2NkJyl9ICRwcm9qZWN0RGlyZWN0b3J5JyksDQogICAgICAgICgpID0+IGxvZ2dlci5pbmZvKCcke2xpZ2h0Q3lhbi53cmFwKCdkYXJ0X2Zyb2cgZGV2Jyl9JyksDQogICAgICBdKTsNCiAgICB9KTsNCg0KICAgIHRlc3QoJ3J1biB0aHJvd3Mgd2hlbiBwdWJzcGVjLnlhbWwgZG9lcyBub3QgZXhpc3QnLCAoKSBhc3luYyB7DQogICAgICBjb250ZXh0LnZhcnMgPSB7J291dHB1dF9kaXJlY3RvcnknOiAnL2ludmFsaWQnfTsNCiAgICAgIGF3YWl0IGV4cGVjdExhdGVyKCgpID0+IHJ1bihjb250ZXh0KSwgdGhyb3dzQShpc0E8RXhjZXB0aW9uPigpKSk7DQogICAgfSk7DQogIH0pOw0KfQ0K",
+      "type": "text",
+    },
+  ],
+  "name": "create_dart_frog",
+  "description": "A Dart Frog starter app template",
+  "version": "0.1.0+1",
+  "environment": {"mason": "^0.1.0"},
+  "readme": {
+    "path": "README.md",
+    "data":
+        "IyBjcmVhdGVfZGFydF9mcm9nDQoNClshW1Bvd2VyZWQgYnkgTWFzb25dKGh0dHBzOi8vaW1nLnNoaWVsZHMuaW8vZW5kcG9pbnQ/dXJsPWh0dHBzJTNBJTJGJTJGdGlueXVybC5jb20lMkZtYXNvbi1iYWRnZSldKGh0dHBzOi8vZ2l0aHViLmNvbS9mZWxhbmdlbC9tYXNvbikNCg0KQSBEYXJ0IEZyb2cgc3RhcnRlciBhcHAgdGVtcGxhdGUNCg0KX0dlbmVyYXRlZCBieSBbbWFzb25dWzFdIPCfp7FfDQoNClsxXTogaHR0cHM6Ly9naXRodWIuY29tL2ZlbGFuZ2VsL21hc29uDQo=",
+    "type": "text",
+  },
+  "changelog": {
+    "path": "CHANGELOG.md",
+    "data":
+        "IyAwLjEuMCsxDQoNCi0gZmVhdDogYmFzaWMgZGFydCBmcm9nIHN0YXJ0ZXIgdGVtcGxhdGUNCg==",
+    "type": "text",
+  },
+  "license": {
+    "path": "LICENSE",
+    "data":
+        "TUlUIExpY2Vuc2UNCg0KQ29weXJpZ2h0IChjKSAyMDIyIFZlcnkgR29vZCBWZW50dXJlcw0KQ29weXJpZ2h0IChjKSAyMDI1IERhcnQgRnJvZyBEZXYNCg0KUGVybWlzc2lvbiBpcyBoZXJlYnkgZ3JhbnRlZCwgZnJlZSBvZiBjaGFyZ2UsIHRvIGFueSBwZXJzb24gb2J0YWluaW5nIGEgY29weQ0Kb2YgdGhpcyBzb2Z0d2FyZSBhbmQgYXNzb2NpYXRlZCBkb2N1bWVudGF0aW9uIGZpbGVzICh0aGUgIlNvZnR3YXJlIiksIHRvIGRlYWwNCmluIHRoZSBTb2Z0d2FyZSB3aXRob3V0IHJlc3RyaWN0aW9uLCBpbmNsdWRpbmcgd2l0aG91dCBsaW1pdGF0aW9uIHRoZSByaWdodHMNCnRvIHVzZSwgY29weSwgbW9kaWZ5LCBtZXJnZSwgcHVibGlzaCwgZGlzdHJpYnV0ZSwgc3VibGljZW5zZSwgYW5kL29yIHNlbGwNCmNvcGllcyBvZiB0aGUgU29mdHdhcmUsIGFuZCB0byBwZXJtaXQgcGVyc29ucyB0byB3aG9tIHRoZSBTb2Z0d2FyZSBpcw0KZnVybmlzaGVkIHRvIGRvIHNvLCBzdWJqZWN0IHRvIHRoZSBmb2xsb3dpbmcgY29uZGl0aW9uczoNCg0KVGhlIGFib3ZlIGNvcHlyaWdodCBub3RpY2UgYW5kIHRoaXMgcGVybWlzc2lvbiBub3RpY2Ugc2hhbGwgYmUgaW5jbHVkZWQgaW4gYWxsDQpjb3BpZXMgb3Igc3Vic3RhbnRpYWwgcG9ydGlvbnMgb2YgdGhlIFNvZnR3YXJlLg0KDQpUSEUgU09GVFdBUkUgSVMgUFJPVklERUQgIkFTIElTIiwgV0lUSE9VVCBXQVJSQU5UWSBPRiBBTlkgS0lORCwgRVhQUkVTUyBPUg0KSU1QTElFRCwgSU5DTFVESU5HIEJVVCBOT1QgTElNSVRFRCBUTyBUSEUgV0FSUkFOVElFUyBPRiBNRVJDSEFOVEFCSUxJVFksDQpGSVRORVNTIEZPUiBBIFBBUlRJQ1VMQVIgUFVSUE9TRSBBTkQgTk9OSU5GUklOR0VNRU5ULiBJTiBOTyBFVkVOVCBTSEFMTCBUSEUNCkFVVEhPUlMgT1IgQ09QWVJJR0hUIEhPTERFUlMgQkUgTElBQkxFIEZPUiBBTlkgQ0xBSU0sIERBTUFHRVMgT1IgT1RIRVINCkxJQUJJTElUWSwgV0hFVEhFUiBJTiBBTiBBQ1RJT04gT0YgQ09OVFJBQ1QsIFRPUlQgT1IgT1RIRVJXSVNFLCBBUklTSU5HIEZST00sDQpPVVQgT0YgT1IgSU4gQ09OTkVDVElPTiBXSVRIIFRIRSBTT0ZUV0FSRSBPUiBUSEUgVVNFIE9SIE9USEVSIERFQUxJTkdTIElOIFRIRQ0KU09GVFdBUkUuDQo=",
+    "type": "text",
+  },
+  "vars": {
+    "name": {
+      "type": "string",
+      "description": "Your application name",
+      "default": "example",
+      "prompt": "What is your app name?",
+    },
+  },
+});
